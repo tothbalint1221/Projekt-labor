005a26512c6efac80977f191606ce21ad8614c72
tothbalint1221 <tothbalint1221@gmail.com>
2026-09-26 18:00:07 +0200

    feat(frontend): add menu and demo sign-in navigation

--- Frontend/MainWindow.xaml.cs
+++ Frontend/MainWindow.xaml.cs
@@ -19,6 +19,23 @@ public partial class MainWindow : Window
 
     private void LoginButton_Click(object sender, RoutedEventArgs e)
     {
+        if(string.IsNullOrWhiteSpace(UsernameTextBox.Text))
+        {
+            MessageBox.Show("Please enter your username");
+            return;
+        }
+
+
+        if (string.IsNullOrWhiteSpace(PasswordInput.Password))
+        {
+            MessageBox.Show("Please enter your password");
+            //MessageBox.Show("Please enter a password.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            return; 
+        }
+
+        var menu = new Frontend.MenuWindow();
+        menu.Show(); //ez nyitja meg a menut
+        Close(); //Ez bezárja a logint, fontos!
 
     }
 }
---
+++ Frontend/MenuWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ServiceManagerApp.Frontend
+{
+    /// <summary>
+    /// Interaction logic for MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CustomersButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void ServiceTicketsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PartsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
